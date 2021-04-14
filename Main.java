f6a1398fb37a50fee3372f31d691977f2c6f87c6
Kyle McDowell <>
2021-04-14 15:19:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    // Activity 1: Call the sentimentVal method in Review with a word like "terrible" and print out the result
+    Review rev = new Review();
+    System.out.println(rev.sentimentVal("terrible"));
+    
+    
+  }
+}
